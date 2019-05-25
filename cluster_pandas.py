34cf0162aee7ffce3e5b482569abec6c15c22396
Kristian Koschany <48453927+kkoschany@users.noreply.github.com>
2019-05-24 22:21:51 -0400

    Add files via upload
    Week 3, Part 1

---
+++ cluster_pandas.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Wed May 22 06:28:19 2019
+
+@author: kkosc
+"""
+
+import pandas as pd
+import wikipedia as wp
+
+#read and scrape the table from Wikipedia
+html = wp.page("List of postal codes of Canada: M").html().encode("UTF-8")
+df = pd.read_html(html)[0]
+
+#adjust the data in the dataframe to our specifications
+df = df[df.Borough != 'Not assigned']
+df = df.groupby(['Postcode','Borough'])['Neighbourhood'].apply(', '.join).reset_index()
+df = df.replace(to_replace='Not assigned', value='Queen\'s Park')
+
+#print the dataframe and its size
+print(df)
+print(df.shape)
